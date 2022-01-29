4a4575e331afa0903bb9c27d545137010181f66f
Muhammad Faisal Amir <faisalamircs@gmail.com>
2022-01-29 10:56:35 +0700

    add: codemr

---
+++ codemr/nf-testcase-googlesample-consume-library/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,35 @@
+var matrix = [[0,0,0,0,2,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,1,0,0,0,2,0,0],[0,0,0,0,1,0,0,1,1,0,0],[0,0,0,0,0,0,0,1,3,0,0],[0,0,0,0,1,0,0,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,0],[2,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,1,0,0,0,0,0,0],[0,0,0,0,1,0,0,1,3,0,0],[0,0,0,0,1,0,0,0,0,0,0]]
+var packages = [{
+"name": " com.example.android.architecture.blueprints.todoapp", "color": " #3182bd"
+}
+,{
+"name": " com.example.android.architecture.blueprints.todoapp.data.source.remote", "color": " #6baed6"
+}
+,{
+"name": " com.example.android.architecture.blueprints.todoapp.data.source.local", "color": " #9ecae1"
+}
+,{
+"name": " com.example.android.architecture.blueprints.todoapp.taskdetail", "color": " #c6dbef"
+}
+,{
+"name": " com.example.android.architecture.blueprints.todoapp.data.source", "color": " #e6550d"
+}
+,{
+"name": " com.example.android.architecture.blueprints.todoapp.addedittask", "color": " #fd8d3c"
+}
+,{
+"name": " com.example.android.architecture.blueprints.todoapp.di", "color": " #fdae6b"
+}
+,{
+"name": " com.example.android.architecture.blueprints.todoapp.util", "color": " #fdd0a2"
+}
+,{
+"name": " com.example.android.architecture.blueprints.todoapp.data", "color": " #31a354"
+}
+,{
+"name": " com.example.android.architecture.blueprints.todoapp.tasks", "color": " #74c476"
+}
+,{
+"name": " com.example.android.architecture.blueprints.todoapp.statistics", "color": " #a1d99b"
+}
+];
---
+++ codemr/nf-testcase-googlesample-consume-library/html/workingsets.js
@@ -0,0 +1,3 @@
+var EQ_workingSetList = [
+{name: 'nf-testcase-googlesample-consume-library.app', path:'nf-testcase-googlesample-consume-libraryapp'}
+];
